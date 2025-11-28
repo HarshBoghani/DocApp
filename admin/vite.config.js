b07e498e7db9dbb53412657ff14ec99348ec53d1
HarshBoghani <harshboghani775@gmail.com>
2025-11-28 17:54:52 +0530

    base path in vite

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/admin/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   server: { port: 5174 },
   extend: {
